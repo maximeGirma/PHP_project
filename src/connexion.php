96b1391b8f3786e60b9d4a8c9a3afa06b5a1001f
maximeGirma <maxime.girma@hotmail.fr>
2018-01-07 12:34:29 +0100

    max test hashage

--- src/connexion.php
+++ src/connexion.php
@@ -1,5 +1,8 @@
 <?php
 echo 'pouet';
-echo $_POST['ID'];
-echo $_POST['PSW'];
+echo $_POST['ID'].'<br/>';
+echo $_POST['PSW'].'<br/>';
+
+$password = password_hash('admin',PASSWORD_DEFAULT);
+echo $password;
 ?>
