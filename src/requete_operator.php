1d80128a709eb06c6af35f52f62fb49bb5d24704
maximeGirma <maxime.girma@hotmail.fr>
2018-01-08 16:04:20 +0100

    aaaaah

--- src/requete_operator.php
+++ src/requete_operator.php
@@ -1,7 +1,7 @@
 <?php 
 //effectue des requetes SQL en fonction de l'argument recu
-echo 'pouet';
-$normal_query =
+//1 - afficher les propriétés des usagers
+$normal_query = array(
 "SELECT
 personnes.prenom
 AS 'PRENOM'
@@ -37,7 +37,50 @@ INNER JOIN type_telephone ON telephone.id_type_tel = type_telephone.id_type_tel
 INNER JOIN habite ON personnes.id_personne = habite.id_personne
 INNER JOIN adresse ON habite.id_adresse = adresse.id_adresse
 INNER JOIN ville_cp ON adresse.id_ville = ville_cp.id_ville
-ORDER BY personnes.nom ASC;";
+ORDER BY personnes.nom ASC;",
+"SELECT 
+
+		year(now()),count(distinct carte_abonnement.id_personne)
+			
+	FROM 
+		carte_abonnement
+		
+		INNER JOIN personnes ON carte_abonnement.id_personne = personnes.id_personne
+		LEFT JOIN resilie ON carte_abonnement.id_abonnement = resilie.id_abonnement
+	
+	WHERE
+		personnes.naissance > (now() - interval 18 year)
+	AND
+		carte_abonnement.date_fin_validite > now()
+	AND 
+		date_resiliation is null
+		
+	ORDER BY personnes.nom;"
+
+,
+"SELECT
+personnes.id_personne AS 'USAGER No',
+personnes.prenom AS 'PRENOM',
+personnes.nom AS 'NOM',
+date_format(personnes.naissance,'%d%m%Y') AS 'DATE DE NAISSANCE',
+concat(adresse.num_rue
+,' ',
+adresse.rue
+,' ',
+ifnull(residence, ''),
+ifnull(batiment, '')) AS 'ADRESSE',
+nom_commune AS 'VILLE',
+code_post as 'CODE POSTAL'
+FROM carte_abonnement
+INNER JOIN personnes ON carte_abonnement.id_personne = personnes.id_personne
+INNER JOIN habite ON personnes.id_personne = habite.id_personne
+INNER JOIN adresse ON habite.id_adresse = adresse.id_adresse
+INNER JOIN ville_cp ON adresse.id_ville = ville_cp.id_ville
+LEFT JOIN resilie ON carte_abonnement.id_abonnement = resilie.id_abonnement
+WHERE carte_abonnement.date_fin_validite > now() AND resilie.date_resiliation is null
+ORDER BY personnes.id_personne;
+;"
+);
 
 require_once '../config.inc.php';
 
@@ -56,20 +99,29 @@ if(isset($_POST['predifined_query'])){
 	echo 'kabla1';
 	if ($request == 1){
 		echo 'kabla2';
-		$statement = $db->prepare($normal_query);
+		$statement = $db->prepare($normal_query[0]);
 		if ($statement->execute()) {
 			echo'requete reussie <br/>!';
 			//$item = $statement->fetchObject();
 			while($item = $statement->fetchObject()){
 				echo $item->NOM;
 			}
-			}
-		else{echo 'pas kabla';}
-	
+		}
 	}
-
+	else if($request==3){
+		$statement = $db->prepare($normal_query[2]);
+		if ($statement->execute()) {
+			echo'requete reussie <br/>!';
+			//$item = $statement->fetchObject();
+			while($item = $statement->fetchObject()){
+				echo $item->NOM;
+			}
+		}
+	}
+	else{echo 'pas kabla';}
+	
 }
 
- ?>
+?>
 
 
