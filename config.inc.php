910c3a6ca4c0dd8c2386fcde0ebfc2c4161ce6c1
zoran07 <zoran.p@free.fr>
2018-01-12 16:39:31 +0100

    Zor import css/html

---
+++ config.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ LE CHEVAL LE CHEVAL C'EST GENIAL ! !
+*/
+ini_set('display_errors','On');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'stapa');
+
+/* pas obligés de fermer la balise */
+?>
