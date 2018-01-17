e23e8aec220fb979f608d1d61496dad17cccbd9e
zoran07 <zoran.p@free.fr>
2018-01-17 17:07:58 +0100

    zor update session timeout

--- src/session.php
+++ src/session.php
@@ -1,19 +1,36 @@
 
 <?php
 
-function init_session($id_utilisateur="", $id_type_utilisateur="", $prenom_utilisateur="", $nom_utilisateur=""){
+function init_session($id_utilisateur="", $id_type_utilisateur="", $prenom_utilisateur="", $nom_utilisateur="") {
 
-	$a = session_id();
-	if(empty($a)) session_start();
+	$SID = session_id();
+	
+	if(empty($SID)) {
+		session_start();
+	}
+		
+	//echo 'Bienvenue ' . $_SESSION['prenom_utilisateur'] . $_SESSION['nom_utilisateur'], '.';
+	//echo "SID: ".SID."<br>session_id(): ".session_id()."<br>COOKIE: ".$_COOKIE["PHPSESSID"];
+
 	
 	$_SESSION['id_utilisateur'] = $id_utilisateur;
 	$_SESSION['id_type_utilisateur'] = $id_type_utilisateur;
 	$_SESSION['prenom_utilisateur'] = $prenom_utilisateur;
 	$_SESSION['nom_utilisateur'] = $nom_utilisateur;
+	$_SESSION['started_time'] = time();
+	$_SESSION['timeout'] = 600;
 	$_SESSION['connected'] = TRUE;
-
-	//echo 'Bienvenue ' . $_SESSION['prenom_utilisateur'] . $_SESSION['nom_utilisateur'], '.';
-	//echo "SID: ".SID."<br>session_id(): ".session_id()."<br>COOKIE: ".$_COOKIE["PHPSESSID"];
-
 }
 
+
+function timeout_session(){
+	
+	$_SESSION['started_time'] = time();
+	$_SESSION['timeout'] = 600;
+	$_SESSION['connected'] = TRUE;
+
+	if(time() > $started_time + $timeout)	{
+		$connected = FALSE;
+		header('Location: end_session.php');
+	}	
+}
