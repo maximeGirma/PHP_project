a185a2be67ad2ef13a0d3db9f1457c53a6c100d8
zoran07 <zoran.p@free.fr>
2018-01-22 09:57:33 +0100

    zor css 2.01

--- config.inc.php
+++ config.inc.php
@@ -6,7 +6,7 @@
 
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'Mouchoir123');
+define('DB_PASS', '');
 define('DB_NAME', 'STAPA');
 
 /* pas obligés de fermer la balise */
